935c8e718dd47b1e2b1aa323ee1844a9e042adf1
holi <hongrui.li@epfl.ch>
2025-09-08 15:23:39 +0200

    [Download] Get the sources for week 1

---
+++ .scala-build/CS-214-excercises_4eaa004db2/src_generated/main/01 - Structural Recursion and Version Control/playground.worksheet.scala
@@ -0,0 +1,50 @@
+package `01$u0020$minus$u0020Structural$u0020Recursion$u0020and$u0020Version$u0020Control`
+
+
+final class playground$u002Eworksheet$_ {
+def args = playground$u002Eworksheet_sc.args$
+def scriptPath = """01 - Structural Recursion and Version Control/playground.worksheet.sc"""
+/*<script>*/
+import recursion.*
+
+2 + 2
+
+val oneTwoMinusThreeFour = IntCons(1, IntCons(2, IntCons(-3, IntCons(4, IntNil()))))
+
+oneTwoMinusThreeFour.isEmpty
+
+oneTwoMinusThreeFour.head
+
+oneTwoMinusThreeFour.tail
+
+oneTwoMinusThreeFour.tail.head
+
+// The call below currently fails with a `NotImplementedError`. Go to
+// `src/main/scala/recursion/listOps.scala`, implement the `length` function,
+// and uncomment the call below to test your implementation.
+
+// length(oneTwoMinusThreeFour)
+
+/*</script>*/ /*<generated>*//*</generated>*/
+}
+
+object playground$u002Eworksheet_sc {
+  private var args$opt0 = Option.empty[Array[String]]
+  def args$set(args: Array[String]): Unit = {
+    args$opt0 = Some(args)
+  }
+  def args$opt: Option[Array[String]] = args$opt0
+  def args$: Array[String] = args$opt.getOrElse {
+    sys.error("No arguments passed to this script")
+  }
+
+  lazy val script = new playground$u002Eworksheet$_
+
+  def main(args: Array[String]): Unit = {
+    args$set(args)
+    val _ = script.hashCode() // hashCode to clear scalac warning about pure expression in statement position
+  }
+}
+
+export playground$u002Eworksheet_sc.script as `playground.worksheet`
+
