808955b20a1693b96cd4be33f585ab332beb8d82
holi <hongrui.li@epfl.ch>
2025-09-08 15:41:04 +0200

    [code] Complete several exercises in week 1

--- 01 - Structural Recursion and Version Control/1_recursion_on_lists.scala
+++ 01 - Structural Recursion and Version Control/1_recursion_on_lists.scala
@@ -1,3 +1,21 @@
 import recursion.*
 
 // length
+def length(l: IntList): Int = 
+    if l.isEmpty then 0 else length(l.tail)
+
+//allPositiveOrZero => ReCheck
+def allPositiveOrZero(l: IntList): Boolean =
+    if l.isEmpty then true else (if l.head <=0 then false else allPositiveOrZero(l.tail))
+
+//countPositive
+def countPositive(l: IntList): Int =
+    if l.isEmpty then 0 else (l.head > 0) + countPositive(l.tail)
+
+//sum
+def sum(l: IntList): Int =
+    if l.isEmpty then 0 else l.head + sum(l.tail)
+
+//product
+def product(l: IntList): Int =
+    if 
