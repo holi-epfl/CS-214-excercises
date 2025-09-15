3da96f62fb1483ef1ea4748b79eb65c7ba8c4c5c
holi-epfl <hongrui.li@epfl.ch>
2025-09-15 20:52:03 +0200

    [code] initial version of 1.1

--- 02 - HOF and debugging/higher-order-functions/src/main/scala/hofs/listOps.scala
+++ 02 - HOF and debugging/higher-order-functions/src/main/scala/hofs/listOps.scala
@@ -1,185 +1,88 @@
 package hofs
 
-def length(l: IntList): Int =
-  if l.isEmpty then 0
-  else 1 + length(l.tail)
+def associative(base: Int, fn: (Int, Int) => Int, l: IntList): Int = 
+  if l.isEmpty then base
+  else fn(l.head, associative(base, fn, l.tail))
 
-def allPositiveOrZero(l: IntList): Boolean =
-  if l.isEmpty then true
-  else l.head >= 0 && allPositiveOrZero(l.tail)
+def length(l: IntList): Int = associative(0, (x, acc) => 1 + acc, l)
 
-def countPositive(l: IntList): Int =
-  if l.isEmpty then 0
-  else (if l.head > 0 then 1 else 0)
-         + countPositive(l.tail)
+def produce(l: IntList): Int = associative(1, (x, acc) => x * acc, l)
 
-def sum(l: IntList): Int =
-  if l.isEmpty then 0
-  else l.head + sum(l.tail)
+def countPositive(l: IntList): Int = associative(0, (x, acc) => (if (x > 0) then 1 else 0)+ acc, l)
 
-def product(l: IntList): Int =
-  if l.isEmpty then 1
-  else l.head * product(l.tail)
+def sum(l: IntList): Int = associative(0, (x, acc) => x + acc, l)
 
-def anyOdd(l: IntList): Boolean =
-  if l.isEmpty then false
-  else (l.head % 2 != 0) || anyOdd(l.tail)
+def countEven(l: IntList): Int = associative(0, (x, acc) => (if x % 2 == 0 then 1 else 0) + acc, l)
 
-def decrement(l: IntList): IntList =
+def multiplyOdd(l: IntList): Int = associative(1, (x, acc) => (if x % 2 != 0 then x else 1) * acc, l)
+
+
+def map(fn: Int => Int)(l: IntList): IntList = 
   if l.isEmpty then IntNil()
-  else IntCons(l.head - 1, decrement(l.tail))
+  else IntCons(fn(l.head), map(fn)(l))
 
-def collectEven(l: IntList): IntList =
+def increment(l: IntList) = map(x => x+1)(l)
+
+def decrement(l: IntList) = map(x => x - 1)(l)
+
+def multiplyBy2(l: IntList) = map(x => 2 * x)(l)
+
+def capAtZero(l: IntList) = map(x => if x < 0 then x else 0)(l)
+
+
+def filter(fn: Int => Boolean)(l: IntList): IntList = 
   if l.isEmpty then IntNil()
-  else if l.head % 2 == 0 then
-    IntCons(l.head, collectEven(l.tail))
-  else collectEven(l.tail)
+  else if fn(l.head) then IntCons(l.head, filter(fn)(l.tail)) else filter(fn)(l.tail)
 
-def min(l: IntList): Int =
+def removeOdd(l: IntList) = filter(x => x % 2 == 0)(l)
+
+def removeZeroes(l: IntList) = filter(x => x != 0)(l)
+
+def collectEven(l: IntList) = removeOdd(l)
+
+def collectMultiples(d: Int, l: IntList) = filter(x => x % d ==0)(l)
+
+def intersection(l: IntList, ll: IntList) = filter(x => contains(ll, x))(l)
+
+def difference(l: IntList, ll: IntList) = filter(x => !contains(ll, x))(l)
+
+
+// Can be replaced by associative?
+def foldRight(base: Int, fn: (Int, Int) => Int, l: IntList): Int = 
+  if l.isEmpty then base
+  else fn(l.head, foldRight(base, fn, l.tail)) 
+
+def horner(x: Int, l: IntList) = foldRight(0, (h, n) => h + x * n, l)
+
+
+def reduceRight(fn: (Int, Int) => Int)(l: IntList): Int = 
   if l.isEmpty then
     throw IllegalArgumentException("Empty list!")
-  else if l.tail.isEmpty then l.head
   else
-    val m = min(l.tail)
-    if l.head < m then l.head else m
+    if l.tail.isEmpty then l.head
+    else fn(l.head, reduceRight(fn)(l.tail))
 
-def increment(l: IntList): IntList =
-  if l.isEmpty then IntNil()
-  else IntCons(l.head + 1, increment(l.tail))
+def substract(l: IntList) = reduceRight((x: Int, n: Int) => x - n)(l)
 
-def subtract(l: IntList): Int =
-  if l.isEmpty then
-    throw IllegalArgumentException("Empty list!")
-  else if l.tail.isEmpty then l.head
-  else l.head - subtract(l.tail)
+def last(l: IntList) = reduceRight((x, n) => n)(l)
 
-def removeOdd(l: IntList): IntList =
-  if l.isEmpty then IntNil()
-  else if l.head % 2 != 0 then removeOdd(l.tail)
-  else IntCons(l.head, removeOdd(l.tail))
+def min(l: IntList) = reduceRight((x, n) => if x < n then x else n)(l)
 
-def countEven(l: IntList): Int =
-  if l.isEmpty then 0
-  else (if l.head % 2 == 0 then 1 else 0)
-         + countEven(l.tail)
 
-def countEven2(l: IntList): Int =
-  length(collectEven(l))
+def search(exists: Boolean, fn: Int => Boolean)(l: IntList): Boolean =
+  if l.isEmpty then !exists
+  else 
+    if fn(l.head) then exists
+    else search(exists, fn)(l.tail)
 
-def multiplyBy2(l: IntList): IntList =
-  if l.isEmpty then IntNil()
-  else IntCons(2 * l.head, multiplyBy2(l.tail))
+def allEven(l:IntList) = search(false, x => x % 2 == 0)(l)
 
-def anyNegative(l: IntList): Boolean =
-  if l.isEmpty then false
-  else l.head < 0 || anyNegative(l.tail)
+def allPositiveOrZero(l: IntList) = search(false, x => x >= 0)(l)
 
-def allEven(l: IntList): Boolean =
-  if l.isEmpty then true
-  else (l.head % 2 == 0) && allEven(l.tail)
+def contains(l:IntList, n:Int) = search(true, x => x == n)(l)
 
-def multiplyOdd(l: IntList): Int =
-  if l.isEmpty then 1
-  else
-    val m = if l.head % 2 != 0
-            then l.head else 1
-    m * multiplyOdd(l.tail)
+def anyOdd(l: IntList) = search(true, x => x % 2 != 0)(l)
 
-def horner(x: Int, l: IntList): Int =
-  if l.isEmpty then 0
-  else l.head + x * horner(x, l.tail)
+def anyNegative(l: IntList) = search(true, x => x < 0)(l)
 
-def capAtZero(l: IntList): IntList =
-  if l.isEmpty then IntNil()
-  else IntCons(if l.head > 0 then 0 else l.head,
-               capAtZero(l.tail))
-
-def removeZeroes(l: IntList): IntList =
-  if l.isEmpty then IntNil()
-  else if l.head == 0 then removeZeroes(l.tail)
-  else IntCons(l.head, removeZeroes(l.tail))
-
-def reverseAppend(l1: IntList, l2: IntList): IntList =
-  if l1.isEmpty then l2
-  else reverseAppend(l1.tail, IntCons(l1.head, l2))
-
-def reverse(l: IntList): IntList =
-  reverseAppend(l, IntNil())
-
-def takeWhilePositive(l: IntList): IntList =
-  if l.isEmpty then IntNil()
-  else if l.head > 0 then
-    IntCons(l.head, takeWhilePositive(l.tail))
-  else IntNil()
-
-def append(l1: IntList, l2: IntList): IntList =
-  if l1.isEmpty then l2
-  else IntCons(l1.head, append(l1.tail, l2))
-
-def appendUsingReverseAppend(l1: IntList, l2: IntList): IntList =
-  reverse(reverseAppend(l2, reverse(l1)))
-
-def collectMultiples(d: Int, l: IntList): IntList =
-  if l.isEmpty then IntNil()
-  else if l.head % d == 0 then
-    IntCons(l.head, collectMultiples(d, l.tail))
-  else collectMultiples(d, l.tail)
-
-def last(l: IntList): Int =
-  if l.isEmpty then
-    throw IllegalArgumentException("Empty list!")
-  else if l.tail.isEmpty then l.head
-  else last(l.tail)
-
-def init(l: IntList): IntList =
-  if l.isEmpty then
-    throw IllegalArgumentException("Empty list!")
-  else if l.tail.isEmpty then IntNil()
-  else IntCons(l.head, init(l.tail))
-
-def contains(l: IntList, n: Int): Boolean =
-  if l.isEmpty then false
-  else (l.head == n) || contains(l.tail, n)
-
-def isSubset(l: IntList, L: IntList): Boolean =
-  if l.isEmpty then true
-  else contains(L, l.head) && isSubset(l.tail, L)
-
-def intersection(l: IntList, L: IntList): IntList =
-  if l.isEmpty then IntNil()
-  else if contains(L, l.head) then
-    IntCons(l.head, intersection(l.tail, L))
-  else intersection(l.tail, L)
-
-def difference(l: IntList, L: IntList): IntList =
-  if l.isEmpty then IntNil()
-  else if contains(L, l.head) then
-    difference(l.tail, L)
-  else IntCons(l.head, difference(l.tail, L))
-
-def minMax(l: IntList): (Int, Int) =
-  if l.isEmpty then
-    throw IllegalArgumentException("Empty list!")
-  else if l.tail.isEmpty then
-    (l.head, l.head)
-  else
-    val (min, max) = minMax(l.tail)
-    (scala.math.min(min, l.head), scala.math.max(max, l.head))
-
-val Add = -1
-val Multiply = -2
-
-def polishEval(l: IntList): (Int, IntList) =
-  if l.head >= 0 then
-    (l.head, l.tail)
-  else if l.head == Add then
-    val (leftValue, afterLeft) = polishEval(l.tail)
-    val (rightValue, afterRight) = polishEval(afterLeft)
-    (leftValue + rightValue, afterRight)
-  else if l.head == Multiply then
-    val (leftValue, afterLeft) = polishEval(l.tail)
-    val (rightValue, afterRight) = polishEval(afterLeft)
-    (leftValue * rightValue, afterRight)
-  else
-    throw InvalidOperatorException(l.head)
-
+def isSubset(l: IntList, ll: IntList) = search(false, x => contains(ll, x))
